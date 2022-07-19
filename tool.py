21fdf1a302337a40bf35f6c73f7cca43c012f5a2
arbbas <94612078+arbbas@users.noreply.github.com>
2022-07-19 16:40:18 +0100

    Rules

--- tool.py
+++ tool.py
@@ -5,41 +5,41 @@ from jsonschema import Draft202012Validator
 import rule_engine
 
 
-class Sensor(object):
-    def __init__(self, sensor_id, ip_address, mac_address, hardware_interface, connection_type,
-                 network_protocols, protocol_version, pairing_process, secure_key_storage,
-                 data_storage, power_consumption, electromagnetic_emission, operating_system,
-                 software_version, firmware_version, interfaces, administration, update_process,
-                 reset_functionality, shared_resources, connected_sensors, encryption, authentication,
-                 input_sanitisation, bandwidth, throughput, latency, error_rate):
-        self.sensor_id = sensor_id
-        self.ip_address = ip_address
-        self.mac_address = mac_address
-        self.hardware_interface = hardware_interface
-        self.connection_type = connection_type
-        self.network_protocols = network_protocols
-        self.protocol_version = protocol_version
-        self.pairing_process = pairing_process
-        self.secure_key_storage = secure_key_storage
-        self.data_storage = data_storage
-        self.power_consumption = power_consumption
-        self.electromagnetic_emission = electromagnetic_emission
-        self.operating_system = operating_system
-        self.software_version = software_version
-        self.firmware_version = firmware_version
-        self.interfaces = interfaces
-        self.administration = administration
-        self.update_process = update_process
-        self.reset_functionality = reset_functionality
-        self.shared_resources = shared_resources
-        self.connected_sensors = connected_sensors
-        self.encryption = encryption
-        self.authentication = authentication
-        self.input_sanitisation = input_sanitisation
-        self.bandwidth = bandwidth
-        self.throughput = throughput
-        self.latency = latency
-        self.error_rate = error_rate
+# class Sensor(object):
+#     def __init__(self, sensor_id, ip_address, mac_address, hardware_interface, connection_type,
+#                  network_protocols, protocol_version, pairing_process, secure_key_storage,
+#                  data_storage, power_consumption, electromagnetic_emission, operating_system,
+#                  software_version, firmware_version, interfaces, administration, update_process,
+#                  reset_functionality, shared_resources, connected_sensors, encryption, authentication,
+#                  input_sanitisation, bandwidth, throughput, latency, error_rate):
+#         self.sensor_id = sensor_id
+#         self.ip_address = ip_address
+#         self.mac_address = mac_address
+#         self.hardware_interface = hardware_interface
+#         self.connection_type = connection_type
+#         self.network_protocols = network_protocols
+#         self.protocol_version = protocol_version
+#         self.pairing_process = pairing_process
+#         self.secure_key_storage = secure_key_storage
+#         self.data_storage = data_storage
+#         self.power_consumption = power_consumption
+#         self.electromagnetic_emission = electromagnetic_emission
+#         self.operating_system = operating_system
+#         self.software_version = software_version
+#         self.firmware_version = firmware_version
+#         self.interfaces = interfaces
+#         self.administration = administration
+#         self.update_process = update_process
+#         self.reset_functionality = reset_functionality
+#         self.shared_resources = shared_resources
+#         self.connected_sensors = connected_sensors
+#         self.encryption = encryption
+#         self.authentication = authentication
+#         self.input_sanitisation = input_sanitisation
+#         self.bandwidth = bandwidth
+#         self.throughput = throughput
+#         self.latency = latency
+#         self.error_rate = error_rate
 
 
 # class Network(object):
@@ -84,7 +84,6 @@ def validate_json(json_data):
 
 
 sensorList = []
-sensors = []
 
 with open('input.txt', 'r', encoding='utf-8') as inp:
     for jsonObj in inp:
@@ -93,6 +92,7 @@ with open('input.txt', 'r', encoding='utf-8') as inp:
 
 isValid, msg = validate_json(sensorDict)
 print(msg)
+
 
 # sensor_1 = sensorList[0]
 # sensor_1_obj = Sensor(**sensor_1)
@@ -107,14 +107,14 @@ print(msg)
 # sensor_5_obj = Sensor(**sensor_5)
 
 
-def node_capturing_rule_1():
-    rule_1 = rule_engine.Rule(
+def node_capturing_rules():
+    node_rule_1 = rule_engine.Rule(
         'data_storage == true'
     )
 
-    filter_1 = rule_1.filter(sensorList)
+    filter_1 = node_rule_1.filter(sensorList)
 
-    print("-" * 80)
+    print("-" * 100)
     print("Sensor vulnerability found: Data stored on device")
     print()
     print("Threat: Node capturing - if node captured data might be obtained by an adversary")
@@ -125,32 +125,82 @@ def node_capturing_rule_1():
     for sensor in filter_1:
         print(sensor['sensor_id'])
 
-    print("-" * 80)
+    print("-" * 100)
 
 
-def anti_tamper_rule():
-    rule_1 = rule_engine.Rule(
+def anti_tamper_rules():
+    at_rule_1 = rule_engine.Rule(
         'anti_tamper_destruction == false'
     )
 
-    filter_1 = rule_1.filter(sensorList)
+    filter_1 = at_rule_1.filter(sensorList)
 
-    print("-" * 80)
+    print("-" * 100)
     print("Sensor vulnerability found: Sensor not tamper proof")
     print()
     print("Threat: Node capturing - if node captured data might be obtained by an adversary")
     print("Threat: Denial of Service Attack - Should the sensor be a cluster head, "
           "this may cause data to not reach the sink. \nData from important sensors"
-          "may also be lost")
+          " may also be lost.")
     print()
-    print("Control: Install a sensor with a tamper resistance mechanism")
+    print("Control: Install a sensor with a secure element that has a tamper resistance mechanism")
     print()
     print("Affected Sensors:")
     for sensor in filter_1:
         print(sensor['sensor_id'])
 
-    print("-" * 80)
+    print("-" * 100)
 
 
-node_capturing_rule_1()
-anti_tamper_rule()
+def battery_information_rule():
+    battery_rule_1 = rule_engine.Rule(
+        'accessible_battery_data == false'
+    )
+
+    filter_1 = battery_rule_1.filter(sensorList)
+
+    print("-" * 100)
+    print("Sensor vulnerability found: Battery information not shared")
+    print()
+    print("Threat: Other threats such as Collision, Unfairness and De-synchronisation "
+          "may not be flagged before sensor battery depleted.")
+    print("Threat: Denial of Service Attack - if the sensors battery is"
+          "depleted through other attack vectors, the sensor will no longer be"
+          "able to transmit data.")
+    print()
+    print("Control: Enable battery data transmission if available.")
+    print()
+    print("Affected Sensors:")
+    for sensor in filter_1:
+        print(sensor['sensor_id'])
+
+    print("-" * 100)
+
+
+def communication_rules():
+    comm_rule_1 = rule_engine.Rule(
+         'connection_type == "MiWi'
+     )
+    filter = comm_rule_1.filter(sensorList)
+
+    print("-" * 100)
+    print("Sensor vulnerability found: Sensor using communication protocol 'MiWi'."
+          " 'MiWi' has an issue with version 6.5 and lower. In 6.5,"
+          " full frame counters are validated before message authentication.")
+    print()
+    print("Threat: Denial of Service Attack - Valid packets will not be able to pass through"
+          " the network.")
+    print("Threat: Replay attack in the stack.")
+    print()
+    print("Control: Update all sensors using MiWi to current patched version.")
+    print()
+    print("Affected Sensors:")
+    for sensor in filter:
+        print(sensor['sensor_id'])
+
+    print("-" * 100)
+
+
+node_capturing_rules()
+anti_tamper_rules()
+battery_information_rule()
